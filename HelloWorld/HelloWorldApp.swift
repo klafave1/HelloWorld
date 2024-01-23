667e6537a3ca863c69ab2af47f4210e5c0495295
klafave1 <156860147+klafave1@users.noreply.github.com>
2024-01-23 01:03:54 -0500

    Initial Commit

---
+++ HelloWorld/HelloWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWorldApp.swift
+//  HelloWorld
+//
+//  Created by Karley LaFave on 1/23/24.
+//
+
+import SwiftUI
+
+@main
+struct HelloWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
